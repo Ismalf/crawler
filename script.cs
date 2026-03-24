176976f7a24d2750e6d194d469a26a0fd56406e0
Ismael Martinez <ismalfmp@gmail.com>
2026-03-24 03:47:43 +0000

    Added basic feature flags and parsed html downloaded content with HTMLAgilityPack

--- script.cs
+++ script.cs
@@ -1,7 +1,11 @@
-// Script-style program to scrap a web site
+// Packages
+#:package Microsoft.Extensions.Configuration.Json@10.0.5
+#:package HtmlAgilityPack@1.12.4
 
 using System;
 using System.Text;
+using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
 
 if (args.Length == 0)
 {
@@ -23,8 +27,6 @@ try
     var robots = await client.GetAsync("/robots.txt");
     var robotsResponse = await robots.Content.ReadAsStringAsync();
 
-    Console.WriteLine(robotsResponse);
-
     if (string.Empty.Equals(robotsResponse))
     {
         Console.WriteLine("No robots txt found, crawler is able to make a full scan");
@@ -34,13 +36,37 @@ try
         Console.WriteLine("Robots txt found, crawler is forbidden access to specific resources");
     }
 
-    
+
 
     var site = await client.GetAsync("/");
     var siteStructure = await site.Content.ReadAsStringAsync();
 
-    Console.WriteLine(siteStructure);
+    // load basic feature flags implementation from appsettings.json
+    var root = new ConfigurationBuilder().AddJsonFile("/workspaces/crawler/appsettings.json", false, true).Build();
 
+    var parseHTML = bool.Parse(root.GetSection("features:parseHTML").Value ?? "false");
+    if (parseHTML)
+    {
+        static int CountNodes(HtmlNode node)
+        {
+            if (node.HasChildNodes)
+            {
+                int nodes = 0;
+                foreach (var childNode in node.ChildNodes)
+                {
+                    nodes += CountNodes(childNode);
+                }
+                return nodes;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        HtmlDocument htmlDocument = new();
+        htmlDocument.LoadHtml(siteStructure);
+        Console.WriteLine($"HTML Parsed! Total nodes in document: {CountNodes(htmlDocument.DocumentNode)}");
+    }
 }
 catch (UriFormatException ufe)
 {
