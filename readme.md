7640ec624a96c77bb8fc51c45a29d2a2bbfa2729
Ismael Martinez <ismalfmp@gmail.com>
2026-03-24 02:20:03 +0000

    Version 0.1.0 reached, crawler is able to download html

--- readme.md
+++ readme.md
@@ -1,2 +1,22 @@
 # README
 
+# Roadmap
+
+Next we have the basic roadmap for the crawler, divided in different versions to ensure product evolution tracking.
+
+## 0.1.0
+
+Be able to download and show HTML content of the provided URL. URL must be passed as a parameter for the script.
+
+## 0.2.0
+
+Be able to parse the HTML content and iterate over its structure to find relevant features.
+Implement feature flags for trunk based development.
+
+## 0.3.0
+
+Be able to extract relevant data from the website.
+
+## 0.4.0
+
+Save data to a database to have metrics of usage and recovered data.
--- script.cs
+++ script.cs
@@ -1,3 +1,48 @@
-using System;
+// Script-style program to scrap a web site
 
-Console.WriteLine("Hello world");
+using System;
+using System.Text;
+
+if (args.Length == 0)
+{
+    throw new ArgumentException("URL parameter expected");
+}
+
+try
+{
+    var webSite = new Uri(args[0]);
+
+
+    Console.WriteLine($"Target DNS: {webSite.Authority}");
+    Console.WriteLine($"Testing compliance (robots protocol)");
+
+    var client = new HttpClient
+    {
+        BaseAddress = webSite
+    };
+    var robots = await client.GetAsync("/robots.txt");
+    var robotsResponse = await robots.Content.ReadAsStringAsync();
+
+    Console.WriteLine(robotsResponse);
+
+    if (string.Empty.Equals(robotsResponse))
+    {
+        Console.WriteLine("No robots txt found, crawler is able to make a full scan");
+    }
+    else
+    {
+        Console.WriteLine("Robots txt found, crawler is forbidden access to specific resources");
+    }
+
+    
+
+    var site = await client.GetAsync("/");
+    var siteStructure = await site.Content.ReadAsStringAsync();
+
+    Console.WriteLine(siteStructure);
+
+}
+catch (UriFormatException ufe)
+{
+    Console.WriteLine($"Format exception, could not parse the requested URL: {ufe.Message}");
+}
